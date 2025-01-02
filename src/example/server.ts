4e7bbb18a6d579d088c9919605735a182176552a
glinto <glinto@gmail.com>
2025-01-02 08:14:58 +0100

    create a real server example

--- src/example/server.ts
+++ src/example/server.ts
@@ -1,15 +1,29 @@
-import { FooRequest } from './logic/base.js';
+import { HTTPError } from '../index.js';
 import { ServerLogic } from './logic/implementation.js';
 import { universalEndpoint } from './logic/service.js';
+import { createServer } from 'http';
 
 universalEndpoint.implementation = new ServerLogic();
 
-// Exmple handler usage, this should work
-universalEndpoint.handlers['/foo']
-	.implementation(<FooRequest>{ name: 'John', age: 42 })
-	.then((result) => {
-		console.log(result);
-	})
-	.catch((error) => {
-		console.log('foo error', error);
-	});
+const server = createServer((req, res) => {
+	universalEndpoint
+		.handleIncomingRequest(req)
+		.then((result) => {
+			res.writeHead(result.status, { 'Content-Type': 'application/json' });
+			res.end(result.body);
+		})
+		.catch((error) => {
+			if (error instanceof HTTPError) {
+				res.writeHead(error.status, { 'Content-Type': 'text/plain' });
+				res.end(error.message);
+			} else {
+				console.error('Internal server error', error);
+				res.writeHead(500, { 'Content-Type': 'text/plain' });
+				res.end('Internal server error');
+			}
+		});
+});
+
+server.listen(8081, () => {
+	console.log('Server is running on http://localhost:8081');
+});
