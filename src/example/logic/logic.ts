3b301730cf7614f358fe4af5cb49177f355351d9
glinto <glinto@gmail.com>
2025-02-25 12:52:40 +0100

    adjust examples

--- src/example/logic/logic.ts
+++ src/example/logic/logic.ts
@@ -1,26 +1,26 @@
-import { arrayGuard, endpointStub, numberGuard, stringGuard, UniversalEndpoint } from '../../index.js';
+import { arrayGuard, endpointStub, HTTPRequest, numberGuard, stringGuard, UniversalEndpoint } from '../../index.js';
 import { FooRequest, FooResult, isFooRequest, isFooResult } from './entities.js';
 
 export const { endpointInstance, endpoint } = UniversalEndpoint.create();
 
 export abstract class Logic {
 	@endpoint('POST', '/foo/:age', isFooRequest, isFooResult)
-	async foo(request: FooRequest): Promise<FooResult> {
+	async foo(request: HTTPRequest<FooRequest>): Promise<FooResult> {
 		return endpointStub(request);
 	}
 
 	@endpoint('GET', '/number/:n', numberGuard, stringGuard)
-	async primitiveNumber(request: number): Promise<string> {
+	async primitiveNumber(request: HTTPRequest<number>): Promise<string> {
 		return endpointStub(request);
 	}
 
 	@endpoint('POST', '/max/:num/:num', arrayGuard(numberGuard), numberGuard)
-	async max(nums: number[]): Promise<number> {
+	async max(nums: HTTPRequest<number[]>): Promise<number> {
 		return endpointStub(nums);
 	}
 
 	@endpoint('GET', '/bar', undefined, stringGuard)
-	async bar(v: void): Promise<string> {
+	async bar(v: HTTPRequest<void>): Promise<string> {
 		return endpointStub(v);
 	}
 }
--- src/example/logic/server.ts
+++ src/example/logic/server.ts
@@ -1,17 +1,18 @@
+import { getPayload, HTTPRequest } from '../../index.js';
 import { FooRequest, FooResult } from './entities.js';
 import { Logic } from './logic.js';
 
 export class ServerLogic extends Logic {
-	async foo(request: FooRequest): Promise<FooResult> {
-		return { message: `Hello, ${request.name}! You are ${request.age} years old` };
+	async foo(request: HTTPRequest<FooRequest>): Promise<FooResult> {
+		return { message: `Hello, ${getPayload(request).name}! You are ${getPayload(request).age} years old` };
 	}
 
-	async primitiveNumber(request: number): Promise<string> {
-		return `You sent ${request}`;
+	async primitiveNumber(request: HTTPRequest<number>): Promise<string> {
+		return `You sent ${getPayload(request)}`;
 	}
 
-	async max(nums: number[]): Promise<number> {
-		return Math.max(...nums);
+	async max(nums: HTTPRequest<number[]>): Promise<number> {
+		return Math.max(...getPayload(nums));
 	}
 
 	async bar(): Promise<string> {
